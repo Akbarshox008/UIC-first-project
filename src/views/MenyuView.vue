b5bca6bf014aa6f79873adb832ff4edadd0bd1ad
Akbarshox008 <akbarsafarov915@gmail.com>
2025-07-17 00:09:26 +0500

    fixed all categories all

---
+++ src/views/MenyuView.vue
@@ -0,0 +1,11 @@
+<template >
+    <div class="container">
+        <Menu/>
+    </div>
+</template>
+<script setup>
+import Menu from "@/components/Menyu/Menu.vue";
+</script>
+<style scoped>
+    
+</style>
